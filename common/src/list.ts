090eb53945f7411ecf679d4c9f5679c6f6f7bca9
Jung Ervin <jung.ervin@gmail.om>
2025-02-16 08:22:08 +0100

    un

--- common/src/list.ts
+++ common/src/list.ts
@@ -1,57 +1,57 @@
-// export class List<T> { 
-//     private items: T[] = [];
-//     private selectedIndex: number | null = null;
+export class List<T> { 
+    private items: T[] = [];
+    private selectedIndex: number | null = null;
 
-//     add(item: T): void {
-//         this.items.push(item);
-//     }
+    add(item: T): void {
+        this.items.push(item);
+    }
 
-//     remove(item: T): boolean {
-//         const index = this.items.indexOf(item);
-//         if (index !== -1) {
-//             this.items.splice(index, 1);
-//             if (this.selectedIndex === index) {
-//                 this.selectedIndex = null;
-//             } else if (this.selectedIndex !== null && this.selectedIndex > index) {
-//                 this.selectedIndex--;
-//             }
-//             return true;
-//         }
-//         return false;
-//     }
+    remove(item: T): boolean {
+        const index = this.items.indexOf(item);
+        if (index !== -1) {
+            this.items.splice(index, 1);
+            if (this.selectedIndex === index) {
+                this.selectedIndex = null;
+            } else if (this.selectedIndex !== null && this.selectedIndex > index) {
+                this.selectedIndex--;
+            }
+            return true;
+        }
+        return false;
+    }
 
-//     indexOf(item: T): number {
-//         return this.items.indexOf(item);
-//     }
+    indexOf(item: T): number {
+        return this.items.indexOf(item);
+    }
 
-//     set selectedElement(item: T | null) {
-//         if (item === null) {
-//             this.selectedIndex = null;
-//         } else {
-//             const index = this.indexOf(item);
-//             if (index !== -1) {
-//                 this.selectedIndex = index;
-//             } else {
-//                 throw new Error("Element not found in the list");
-//             }
-//         }
-//     }
+    set selectedElement(item: T | null) {
+        if (item === null) {
+            this.selectedIndex = null;
+        } else {
+            const index = this.indexOf(item);
+            if (index !== -1) {
+                this.selectedIndex = index;
+            } else {
+                throw new Error("Element not found in the list");
+            }
+        }
+    }
 
-//     get selectedElement(): T | null {
-//         return this.selectedIndex !== null ? this.items[this.selectedIndex] : null;
-//     }
+    get selectedElement(): T | null {
+        return this.selectedIndex !== null ? this.items[this.selectedIndex] : null;
+    }
 
 
-//     get allItems(): T[] {
-//         return [...this.items]; 
-//     }
+    get allItems(): T[] {
+        return [...this.items]; 
+    }
 
-//     clear(): void {
-//         this.items = [];
-//         this.selectedIndex = null;
-//     }
+    clear(): void {
+        this.items = [];
+        this.selectedIndex = null;
+    }
 
-//     get size(): number {
-//         return this.items.length;
-//     }
-// }
+    get size(): number {
+        return this.items.length;
+    }
+}
--- dist/js/common/src/list.js
+++ dist/js/common/src/list.js
@@ -1,49 +1,59 @@
-"use strict";
-// export class List<T> { 
-//     private items: T[] = [];
-//     private selectedIndex: number | null = null;
-//     add(item: T): void {
-//         this.items.push(item);
-//     }
-//     remove(item: T): boolean {
-//         const index = this.items.indexOf(item);
-//         if (index !== -1) {
-//             this.items.splice(index, 1);
-//             if (this.selectedIndex === index) {
-//                 this.selectedIndex = null;
-//             } else if (this.selectedIndex !== null && this.selectedIndex > index) {
-//                 this.selectedIndex--;
-//             }
-//             return true;
-//         }
-//         return false;
-//     }
-//     indexOf(item: T): number {
-//         return this.items.indexOf(item);
-//     }
-//     set selectedElement(item: T | null) {
-//         if (item === null) {
-//             this.selectedIndex = null;
-//         } else {
-//             const index = this.indexOf(item);
-//             if (index !== -1) {
-//                 this.selectedIndex = index;
-//             } else {
-//                 throw new Error("Element not found in the list");
-//             }
-//         }
-//     }
-//     get selectedElement(): T | null {
-//         return this.selectedIndex !== null ? this.items[this.selectedIndex] : null;
-//     }
-//     get allItems(): T[] {
-//         return [...this.items]; 
-//     }
-//     clear(): void {
-//         this.items = [];
-//         this.selectedIndex = null;
-//     }
-//     get size(): number {
-//         return this.items.length;
-//     }
-// }
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.List = void 0;
+    class List {
+        constructor() {
+            this.items = [];
+            this.selectedIndex = null;
+        }
+        add(item) {
+            this.items.push(item);
+        }
+        remove(item) {
+            const index = this.items.indexOf(item);
+            if (index !== -1) {
+                this.items.splice(index, 1);
+                if (this.selectedIndex === index) {
+                    this.selectedIndex = null;
+                }
+                else if (this.selectedIndex !== null && this.selectedIndex > index) {
+                    this.selectedIndex--;
+                }
+                return true;
+            }
+            return false;
+        }
+        indexOf(item) {
+            return this.items.indexOf(item);
+        }
+        set selectedElement(item) {
+            if (item === null) {
+                this.selectedIndex = null;
+            }
+            else {
+                const index = this.indexOf(item);
+                if (index !== -1) {
+                    this.selectedIndex = index;
+                }
+                else {
+                    throw new Error("Element not found in the list");
+                }
+            }
+        }
+        get selectedElement() {
+            return this.selectedIndex !== null ? this.items[this.selectedIndex] : null;
+        }
+        get allItems() {
+            return [...this.items];
+        }
+        clear() {
+            this.items = [];
+            this.selectedIndex = null;
+        }
+        get size() {
+            return this.items.length;
+        }
+    }
+    exports.List = List;
+});
