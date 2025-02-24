21711dd2c63be3fbbdb67596d2722703a06e7383
jungervin <jung.ervin@gmail.com>
2025-02-24 08:42:22 +0100

    dispatcher

--- dist/dispatcher.js
+++ dist/dispatcher.js
@@ -4,85 +4,12 @@ if (!App.num) {
     App.num = 0;
 }
 
-function task1() {
-
-    if (!Api.tasks.getTask("Task1")) {
-
-        const task = Api.tasks.addTask("Task1")
-
-        task.setLoco(3)
-
-        // ==========================================
-        //  Szfvár P3 <=== P2 <== Szabadbattyán P3
-        // ==========================================
-        
-        //task.waitForMinute(5)
-        task.startAtMinutes([5, 15, 25, 35, 45, 55])
-        task.setRoute("routeSwitch112")
-        task.waitMs(3000, 5000)
-
-        // Kalauz síp
-        task.setFunctionMs(17, true, 500)
-        task.waitMs(3000, 5000)
-
-        // Hátra és Kürt
-        task.reverse(0)
-        task.setFunctionMs(3, true, 500)
-        task.waitMs(3000, 5000)
-        task.reverse(30)
-
-        task.waitForSensor(16, true)
-
-        task.setFunctionMs(3, true, 500)
-        task.delay(3000)
-        task.stop()
-
-        task.waitMs(10000, 20000)
-
-        // ==========================================
-        //  Szfvár P3 ==> P1 ==> Szabadbattyán P3
-        // ==========================================
-
-        //task.waitForMinute(5)
-        task.startAtMinutes([0, 10, 20, 30, 40, 50])
-        task.setRoute("routeSwitch113")
-        task.waitMs(3000, 5000)
-
-        // Kalauz síp
-        task.setFunctionMs(17, true, 500)
-
-        task.waitMs(3000, 5000)
-
-        // Előre és Kürt
-        task.foward(0)
-        task.setFunctionMs(3, true, 500)
-        task.waitMs(3000, 5000)
-        task.foward(30)
-
-        task.waitForSensor(24, true)
-
-        task.setFunctionMs(3, true, 500)
-
-        task.delay(3000)
-        task.stop()
-
-        task.waitMs(10000, 20000)
-
-        task.restart()
-
-    }
-}
-
 //==================================
 // INIT
 //==================================
 if (!App.init) {
     App.init = true
     App.status = {}
-    task1()
-
-
-
     return
 }
 
@@ -131,12 +58,6 @@ if (rb24 != App.status.rb24) {
 // SIGNAL #50
 //======================================
 {
-    // if(rb12) {
-    //     if (!s50.isRed) {
-    //         s50.sendRed()
-    //     }
-    // } 
-    // else 
     if (!t10.t1Closed && (rb12 || !t16.t1Closed || !t12.t2Closed)) {
         if (!s50.isRed) {
             s50.sendRed()
