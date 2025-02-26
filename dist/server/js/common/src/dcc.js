beac0ca0ffbdaf490f3912c8c17d65b938772e1d
jungervin <jung.ervin@gmail.com>
2025-02-26 10:05:46 +0100

    jsconfig

--- dist/server/js/common/src/dcc.js
+++ dist/server/js/common/src/dcc.js
@@ -9,7 +9,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultSettings = exports.fetchDevices = exports.getUUID = exports.rbus = exports.turnouts = exports.ext_accessories = exports.accessories = exports.locos = exports.setDecoder = exports.getCommandCenterType = exports.CommandCenterTypes = exports.iZ21STATUS = exports.Z21POWERINFO = exports.ApiCommands = exports.DCCExTurnout = exports.DCCExDirections = exports.Z21Directions = exports.SpeedModes = void 0;
+exports.defaultSettings = exports.rbus = exports.turnouts = exports.ext_accessories = exports.accessories = exports.locos = exports.setDecoder = exports.CommandCenterTypes = exports.iZ21STATUS = exports.Z21POWERINFO = exports.ApiCommands = exports.DCCExTurnout = exports.DCCExDirections = exports.Z21Directions = exports.SpeedModes = void 0;
+exports.getCommandCenterType = getCommandCenterType;
+exports.getUUID = getUUID;
+exports.fetchDevices = fetchDevices;
 var SpeedModes;
 (function (SpeedModes) {
     SpeedModes[SpeedModes["S14"] = 0] = "S14";
@@ -96,7 +99,6 @@ function getCommandCenterType(type) {
     const res = Object.keys(CommandCenterTypes).filter(key => isNaN(Number(key)));
     return res ? res[type] : "Unknown";
 }
-exports.getCommandCenterType = getCommandCenterType;
 // export interface iDCCExTCPCommandCenter extends iCommandCenter{
 //     ip: string,
 //     port: number
@@ -123,7 +125,6 @@ exports.rbus = {};
 function getUUID() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c => (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16));
 }
-exports.getUUID = getUUID;
 function fetchDevices() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -136,7 +137,6 @@ function fetchDevices() {
         }
     });
 }
-exports.fetchDevices = fetchDevices;
 exports.defaultSettings = {
     CommandCenter: {
         type: CommandCenterTypes.Z21,
--- dist/server/js/server/src/dccexTCP.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=dccexTCP.js.map
