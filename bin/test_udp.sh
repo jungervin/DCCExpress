5a1bc93ca99c9e2626d083f3b7b84113afbe0939
jungervin <jung.ervin@gmail.com>
2025-03-05 14:03:58 +0100

    helper scripts

--- bin/test_udp.sh
+++ bin/test_udp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-Z21_IP="192.168.1.100"
+Z21_IP="192.168.1.70"
 Z21_PORT=21105
 TIMEOUT=1  # sec
 
-echo "📡 Z21 UDP parancs küldése..."
-echo -ne '\x04\x00\x40\x00\x24\x00' | socat - UDP:$Z21_IP:$Z21_PORT &
+echo "Z21 UDP GET SERIAL NUMBER: $Z21_IP:$Z21_PORT"
+echo -ne '\x04\x00\x10\x00' | socat - UDP:$Z21_IP:$Z21_PORT &
 
 
 RESPONSE=$(timeout $TIMEOUT nc -u -l -p $Z21_PORT)
 
 if [[ -z "$RESPONSE" ]]; then
-    echo "⏳ Timeout: Nem érkezett válasz $TIMEOUT másodpercen belül."
+    echo "⏳ Timeout: $TIMEOUT sec."
 else
-    echo "✅ Válasz érkezett: $RESPONSE"
+    echo "✅ RESPONSE: $RESPONSE"
 fi
