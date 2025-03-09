bf41310e2032baf39ca7eb6edd0bd629d420adfd
jungervin <jung.ervin@gmail.com>
2025-03-09 07:43:34 +0100

    start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,12 @@
-call npm install --force --no-fund
-rem cd dist
-rem node server/js/server/src/index.js
-npm run start
-pause
+@echo off
+
+for /f "delims=" %%v in ('node -v 2^>nul') do set NODE_VERSION=%%v
+
+IF "%NODE_VERSION%"=="" (
+    echo Node.js is not installed! Please install it from https://nodejs.org/
+    pause
+    exit /b 1
+) ELSE (
+    echo Node.js is installed. Version: %NODE_VERSION%
+    npm run start
+)
